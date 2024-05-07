6baf0a92d098519da58f4feb442a447799b179af
Liang Wang <142849399+lwang1517@users.noreply.github.com>
2024-05-07 17:23:24 +0000

    Some changes

---
+++ HelloWorld.py
@@ -0,0 +1,31 @@
+# %% Import packages
+import matplotlib.pyplot as plt
+import numpy as np
+arr = np.array(("my favorite language is Python", "Python"))
+arr
+
+# %%
+print("Hellow World")
+
+# %%
+
+@profile
+def add_or_subtract(n1, n2, subtract=False):
+    if subtract:
+        return n1 - n2
+    else:
+        return n1 + n2
+
+
+results = add_or_subtract(1, 2, True)
+print(results)
+
+results = add_or_subtract(n2=1, n1=2, subtract=True)
+print(results)
+
+# %%
+
+xpts = np.array([1, 3])
+ypts = np.array([2, 9])
+plt.plot(xpts, ypts)
+# %%
